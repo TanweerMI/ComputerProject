17f1201d5151172a6443582fa3ce4e088fd565f7
TanweerMI <154819103+TanweerMI@users.noreply.github.com>
2024-09-13 20:39:20 +0400

    Update CompProject.py

--- CompProject.py
+++ CompProject.py
@@ -4,14 +4,20 @@ from tkinter import ttk
 
 con = mysql.connector.connect(host='localhost', user='root', password='torgius', database='tanweer12k')
 cur = con.cursor()
+cur.execute('delete from receipt')
 
 l = []
 rowno = 1
+confirmed = False
 
 #call this function after scanning, with label name as argument
 def scanned(itemname):
-    global rowno, l
+    global rowno, l, confirmed
 
+    if confirmed == False and rowno!=1:
+        return None
+    
+    confirmed = False
     l.clear()
     quantity = tk.IntVar(window, 1)
 
@@ -45,15 +51,30 @@ def scanned(itemname):
             total_var.set(quantity.get() * float(l[1]))
 
     def rem():
+        cur.execute(f"delete from receipt where item_name = '{l[0]}'")
+        con.commit()
         itna.destroy()
         price.destroy()
         info.destroy()
         qty.destroy()
         total.destroy()
+    
+    def confirm():
+        global confirmed
+        confirmed = True
+        cur.execute(f"insert into receipt values('{l[0]}',{float(l[1])},'{l[2]}',{quantity.get()},{total_var.get()})")
+        con.commit()
+
+    def receipt():
+        if confirmed == False:
+            cur.execute(f"insert into receipt values('{l[0]}',{float(l[1])},'{l[2]}',{quantity.get()},{float(total_var.get())})")
+            con.commit()
 
     qty_increase.config(command=inc)
     qty_decrease.config(command=dec)
     remove_last.config(command=rem)
+    confirm_button.config(command=confirm)
+    receipt_button.config(command=receipt)
 
     rowno += 1
 
@@ -69,11 +90,19 @@ title_label.pack()
 input_frame = ttk.Frame(master=window)
 qty_increase = ttk.Button(master=input_frame, text='INC')
 qty_decrease = ttk.Button(master=input_frame, text='DEC')
-remove_last = ttk.Button(master=input_frame, text='REMOVE') 
+remove_last = ttk.Button(master=input_frame, text='REMOVE')
+test_button = ttk.Button(master=input_frame, text='TESTING',command=lambda: scanned('Regular-Tomato'))
+confirm_button = ttk.Button(master=input_frame, text='CONFIRM')
+receipt_button = ttk.Button(master=input_frame, text='CHECKOUT')
+
 
 qty_increase.grid(row=0, column=0, padx=10)
 qty_decrease.grid(row=0, column=1, padx=10)
 remove_last.grid(row=1, column=0, columnspan=2, pady=10, sticky='we')
+test_button.grid(row=2, column=0, columnspan=2, pady=10, sticky='we')
+confirm_button.grid(row=3, column=0, columnspan=2, pady=10, sticky='we')
+receipt_button.grid(row=4, column=0, columnspan=2, pady=10, sticky='we')
+
 input_frame.pack(side='right')
 
 item_frame = ttk.Frame(master=window)
