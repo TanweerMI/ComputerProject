69676cd20c03b948493a8858e00e6b0f4443e965
TanweerMI <154819103+TanweerMI@users.noreply.github.com>
2024-09-14 10:19:11 +0400

    Update CompProject.py

--- CompProject.py
+++ CompProject.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import tkinter as tk
 from tkinter import ttk
+import ttkbootstrap as ttk
 
 con = mysql.connector.connect(host='localhost', user='root', password='torgius', database='tanweer12k')
 cur = con.cursor()
@@ -21,6 +22,16 @@ def scanned(itemname):
     l.clear()
     quantity = tk.IntVar(window, 1)
 
+    separator1 = ttk.Separator(master=item_frame, orient='vertical')
+    separator2 = ttk.Separator(master=item_frame, orient='vertical')
+    separator3 = ttk.Separator(master=item_frame, orient='vertical')
+    separator4 = ttk.Separator(master=item_frame, orient='vertical')
+
+    separator1.grid(row=rowno, column=1, sticky='ns', padx=5, pady=5)
+    separator2.grid(row=rowno, column=3, sticky='ns', padx=5, pady=5)
+    separator3.grid(row=rowno, column=5, sticky='ns', padx=5, pady=5)
+    separator4.grid(row=rowno, column=7, sticky='ns', padx=5, pady=5)
+
     cur.execute('select * from Items')
     for i in cur.fetchall():
         if i[1] == itemname:
@@ -36,10 +47,10 @@ def scanned(itemname):
     total = ttk.Label(master=item_frame, textvariable=total_var)
 
     itna.grid(row=rowno, column=0)
-    price.grid(row=rowno, column=1)
-    info.grid(row=rowno, column=2)
-    qty.grid(row=rowno, column=3)
-    total.grid(row=rowno, column=4)
+    price.grid(row=rowno, column=2)
+    info.grid(row=rowno, column=4)
+    qty.grid(row=rowno, column=6)
+    total.grid(row=rowno, column=8)
 
     def inc():
         quantity.set(quantity.get() + 1)
@@ -51,6 +62,7 @@ def scanned(itemname):
             total_var.set(quantity.get() * float(l[1]))
 
     def rem():
+        global confirmed, rowno
         cur.execute(f"delete from receipt where item_name = '{l[0]}'")
         con.commit()
         itna.destroy()
@@ -58,6 +70,8 @@ def scanned(itemname):
         info.destroy()
         qty.destroy()
         total.destroy()
+        confirmed = True
+        rowno-=1
     
     def confirm():
         global confirmed
@@ -80,9 +94,9 @@ def scanned(itemname):
 
     item_frame.pack(pady=20)
 
-window = tk.Tk()
+window = ttk.Window(themename = 'darkly')
 window.title('Menu')
-window.geometry('1000x700')
+window.geometry('1100x800')
 
 title_label = ttk.Label(master=window, text='SCANNED ITEMS', font='TimesNewRoman 24 bold')
 title_label.pack()
@@ -96,12 +110,12 @@ confirm_button = ttk.Button(master=input_frame, text='CONFIRM')
 receipt_button = ttk.Button(master=input_frame, text='CHECKOUT')
 
 
-qty_increase.grid(row=0, column=0, padx=10)
-qty_decrease.grid(row=0, column=1, padx=10)
-remove_last.grid(row=1, column=0, columnspan=2, pady=10, sticky='we')
-test_button.grid(row=2, column=0, columnspan=2, pady=10, sticky='we')
-confirm_button.grid(row=3, column=0, columnspan=2, pady=10, sticky='we')
-receipt_button.grid(row=4, column=0, columnspan=2, pady=10, sticky='we')
+qty_increase.grid(row=0, column=0, padx=15, pady=15)
+qty_decrease.grid(row=0, column=1, padx=15, pady=15)
+remove_last.grid(row=1, column=0, columnspan=2, pady=15, sticky='we')
+test_button.grid(row=2, column=0, columnspan=2, pady=15, sticky='we')
+confirm_button.grid(row=3, column=0, columnspan=2, pady=15, sticky='we')
+receipt_button.grid(row=4, column=0, columnspan=2, pady=15, sticky='we')
 
 input_frame.pack(side='right')
 
@@ -114,10 +128,21 @@ qty = ttk.Label(master=item_frame, text='Quantity', font='TimesNewRoman 18 bold'
 total = ttk.Label(master=item_frame, text='Total', font='TimesNewRoman 18 bold')
 
 itemname.grid(row=0, column=0)
-price.grid(row=0, column=1, padx=40)
-info.grid(row=0, column=2, padx=110)
-qty.grid(row=0, column=3, padx=10)
-total.grid(row=0, column=4, padx=10)
+price.grid(row=0, column=2, padx=40)
+info.grid(row=0, column=4, padx=110)
+qty.grid(row=0, column=6, padx=10)
+total.grid(row=0, column=8, padx=10)
+
+separator1 = ttk.Separator(master=item_frame, orient='vertical')
+separator2 = ttk.Separator(master=item_frame, orient='vertical')
+separator3 = ttk.Separator(master=item_frame, orient='vertical')
+separator4 = ttk.Separator(master=item_frame, orient='vertical')
+
+separator1.grid(row=0, column=1, sticky='ns', padx=5, pady=5)
+separator2.grid(row=0, column=3, sticky='ns', padx=5, pady=5)
+separator3.grid(row=0, column=5, sticky='ns', padx=5, pady=5)
+separator4.grid(row=0, column=7, sticky='ns', padx=5, pady=5)
+
 item_frame.pack(side='left')
 
 window.mainloop()
